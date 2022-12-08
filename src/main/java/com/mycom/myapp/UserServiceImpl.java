326e3ec1995bb35b647fa6702159435ce7d5bef9
HurSeungDon <zzaa9999@naver.com>
2022-12-08 20:33:00 +0900

    my first commit

---
+++ src/main/java/com/mycom/myapp/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.mycom.myapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl {
+//    @Autowired
+//    UserDAO userDAO;
+//    public
+}
